b3dada7a4eaa4bf1ee910787fcfc247f952f8bc2
Sufiyan-11 <sufiyanshaikh11121998@gmail.com>
2024-10-30 18:00:32 +0530

    file created

---
+++ 02Basics/06ConditionalLogin.js
@@ -0,0 +1,12 @@
+// code for conditionally login
+
+var email = false;
+var google = false;
+var facebook = true;
+
+if (email || google || facebook) {
+  console.log("Login Sucess");
+} else {
+  console.log("Login Failed");
+}
+
